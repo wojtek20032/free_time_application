bc1c15d29210a2f127860d677429a192e569132a
wojtek20032 <245867@edu.p.lodz.pl>
2024-05-26 15:00:32 +0200

    User class adjusments

--- user.php
+++ user.php
@@ -1,48 +1,55 @@
 <?php
-
 class User {
+    private $id;
     private $name;
     private $email;
     private $password;
 
-    
-    public function __construct($name,$email,$hash) {
+    public function __construct($name, $email, $hash) {
         $this->name = $name;
         $this->email = $email;
         $this->password = $hash;
     }
 
-   
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
     public function getName() {
         return $this->name;
     }
 
-    
     public function setName($name) {
         $this->name = $name;
     }
+
     public function getEmail() {
         return $this->email;
     }
 
-    public function getPassword(){
-        return $this->$password;
-    }
-  
     public function setEmail($email) {
         $this->email = $email;
     }
-    public function login($email, $password,$connection) {
-        $querry = mysqli_query($connection,"SELECT * from users");
-        while($result = mysqli_fetch_array($querry)){
-            if ($result['E_mail'] === $email && $result['hashed_password'] == $password) {
-                return true;
-            }
-        }
-            return false;//połączenie z baza 
-        
+
+    public function setPassword($password) {
+        $this->password = $password;
     }
 
+    public function login($email, $password, $connection) {
+        $query = mysqli_query($connection, "SELECT * FROM users WHERE E_mail = '$email' AND hashed_password = '$password'");
+        if ($result = mysqli_fetch_array($query)) {
+            $this->setId($result['idUzytkownika']);
+            $this->setName($result['Username']);
+            $this->setEmail($result['E_mail']);
+            $this->setPassword($result['hashed_password']);
+            return true;
+        }
+        return false;
+    }
 
     public function changePassword($currentPassword, $newPassword) {
         if (password_verify($currentPassword, $this->password)) {
@@ -53,11 +60,12 @@ class User {
         }
     }
 
-   
     public function changeEmail($newEmail) {
         $this->email = $newEmail;
         return "Email changed successfully!";
     }
 }
+
+
 
 
